08c9ede7304d4985dc89646c353e4c64e77d17fc
Zisis Charalampos <xarisban@gmail.com>
2025-10-14 08:01:05 +0300

    update queries

--- First_queries.sql
+++ First_queries.sql
@@ -71,7 +71,7 @@ with total_rating as
 (
 	select 
 		product_id,
-		sum(rating) as total_rating,
+		sum(rating) as total_rating,z
 		count(rating) as rating_count
 	from reviews
 	group by 1
@@ -82,3 +82,72 @@ select
 from total_rating
 order by 2 desc;
 
+
+
+--7		List all orders along with customer first and last names and order status.
+select 
+	o.order_id,
+	o.customer_id,
+	o.order_status,
+	c.first_name,
+	c.last_name
+from orders as o
+join customers as c
+on c.customer_id = o.customer_id
+group by 1,2,4,5;
+
+
+--8		List products with average rating above 4 along with the number of reviews.
+select 
+	r.product_id,
+	p.product_name,
+	p.category,
+	avg(r.rating) as avg_rating,
+	count(r.review_id) as no_of_reviews
+from reviews as r
+join products as p
+on r.product_id = p.product_id
+group by 1,2,3
+having	avg(r.rating) > 0
+order by avg_rating desc;
+
+
+--9		Get all orders where a review has been submitted (delivered) , including review_text.
+select 
+	o.order_status,
+	r.customer_id,
+	r.review_text
+from orders as o
+join reviews as r
+on r.customer_id = o.customer_id
+where o.order_status = 'Delivered'
+group by 1,2,3
+
+
+
+--10	Find the top 5 customers who spent the most in total.
+select 
+	c.first_name,
+	c.last_name,
+	c.customer_id,
+	(oi.quantity*oi.unit_price) as total_revenue
+from customers as c
+join orders as o
+on c.customer_id = o.customer_id
+join order_items as oi
+on o.order_id = oi.order_id
+group by 3,4
+order by total_revenue desc
+LIMIT 5;
+
+
+--11	Calculate total sales per month.
+select
+	SUM(oi.quantity * oi.unit_price) AS total_sales,
+	extract(month from order_date) as month,
+	extract(year from order_date) as year
+from orders as o
+join order_items as oi
+on o.order_id = oi.order_id
+group by 2,3
+order by 1 desc;
